abf51577f921649960d29ebdebf846f4bc1d5f9d
André Sichelero <19790357+andresichelero@users.noreply.github.com>
2022-12-05 00:12:37 -0300

    Add files via upload

---
+++ Day 4/part 2.py
@@ -0,0 +1,12 @@
+pairs = []
+with open("input.txt", "r+") as f:
+  for line in f.readlines():
+    start1, end1 = map(int, line.split(",")[0].split("-"))
+    start2, end2 = map(int, line.split(",")[1].split("-"))
+    pairs.append((start1, end1, start2, end2))
+
+count = 0
+for (start1, end1, start2, end2) in pairs:
+  if (start1 <= start2 <= end1) or (start1 <= end2 <= end1) or (start2 <= start1 <= end2) or (start2 <= end1 <= end2):
+    count += 1
+print(count)
